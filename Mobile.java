2f8a401b96750ea502660bb8f69239498d8859fa
praveenakupp <88589719+praveenakupp@users.noreply.github.com>
2021-08-07 21:51:35 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,25 @@
+package week1.day1;
+
+public class Mobile {
+
+
+public static void main (String args[])
+{
+	String brand="iPhone";
+	char color='B';
+	int price=4000;
+	byte mem=120;
+	float screen=0.5f;
+	long phoneNo=9953839393L;
+	System.out.println("brand: " + brand);
+	System.out.println("Price of "+brand +" is " + price);
+	System.out.println("mem is 120");
+	System.out.println("screen size is "+0.5f);
+	System.out.println("phone no");
+	
+	
+	
+}
+
+
+}
---
+++ Praveena.java
@@ -0,0 +1,10 @@
+package week1.day1;
+
+public class Praveena {
+
+
+      public static void main(String args[])
+      {
+      System.out.println("Praveena");
+      }
+    }
